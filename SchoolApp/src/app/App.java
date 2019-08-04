56d9db1bcf94b073715fbf4794d246b2dd4c7ae2
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-08-04 20:41:19 +0200

    1 hour into tutorial

--- SchoolApp/src/app/App.java
+++ SchoolApp/src/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.Date;
 public class App {
 
@@ -45,8 +46,60 @@ public class App {
 
 
 
-        String message = "Hello World" + "!!";
+        String message = "   Hello World" + "!!";
         message.endsWith("!!");
-        System.out.println(message.replace("!", "*"));
+        System.out.println(message.trim());
+        System.out.println(message);
+
+        //c:\Windows\...
+        String story = "c:\tWindows\\...";
+        System.out.println(story);
+
+
+
+
+
+
+
+        //! ARRAYS //
+
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        Arrays.toString(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+
+
+        int[] numbers1 = {2, 3, 5, 1, 4};
+        System.out.println(numbers1.length);
+
+        Arrays.sort(numbers1);
+        System.out.println(Arrays.toString(numbers1));
+
+
+
+
+
+        int[][] numbers2 = { {1, 2, 3},{4, 5, 6}};
+        System.out.println(Arrays.deepToString(numbers2));
+
+
+        //*-----------------------------------------//
+        //*-----------------------------------------//
+        //*-----------------------------------------//
+        //*-----------------------------------------//
+        //*-----------------------------------------//
+
+
+        final float pi = 3.14F;
+
+
+
+        double result = (double)10 / (double)3;
+        System.out.println(result);
+
+        
     }
 }
