b1ced2ddadc5f1e4a92b4fa651af320fd94fe808
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-08-04 14:44:39 +0200

    testing other methods of using strings

--- test.java
+++ test.java
@@ -1,6 +1,15 @@
 public class test {
+    private static String x;
+    private static String y;
+    private static String z;
+
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        x = "Hello World";
+        y = "Hello Git";
+        z = "What does this button do?";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
     }
 }
 
