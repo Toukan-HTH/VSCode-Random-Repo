632b93578418915869b0346732d90e87f47cbc8e
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-08-04 19:44:19 +0200

    ø

--- SchoolApp/src/app/App.java
+++ SchoolApp/src/app/App.java
@@ -1,7 +1,52 @@
 package app;
 
+import java.awt.Point;
+import java.util.Date;
 public class App {
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        byte myAge = 30;
+        long viewsCount = 3_212_345_678L;
+        float price =10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+
+
+
+
+
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now); 
+        
+        
+
+
+
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);
+        
+
+
+
+
+
+
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 3;
+        point1.y = 3;
+        System.out.println(point2);
+
+
+
+
+        String message = "Hello World" + "!!";
+        message.endsWith("!!");
+        System.out.println(message.replace("!", "*"));
     }
 }
