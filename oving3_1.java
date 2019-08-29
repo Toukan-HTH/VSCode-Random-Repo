b18e987bba528d71bea6680bb3534c248505e644
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-08-29 13:18:06 +0200

    ferdig 8.29.2019

--- oving3_1.java
+++ oving3_1.java
@@ -3,7 +3,13 @@ class oving3_1{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter tall fra multiplikasjonstabellen");
-        int gange = in.nextInt();
+        String input = in.nextLine();
+        int gange = 0;
+        try{
+            gange = Integer.parseInt(input);
+        }catch (NumberFormatException nfe){
+            nfe.printStackTrace();
+        }
         int resultat;
             for (int i = 0; i<11; i++){
                 resultat=gange*i;
