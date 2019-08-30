94deaeac0b670313af9fa8c3ae8d0ab025b5513f
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-08-30 10:53:48 +0200

    fking ayy øving 3 done

--- oving3_1.java
+++ oving3_1.java
@@ -1,20 +1,43 @@
 import java.util.Scanner;
 class oving3_1{
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter tall fra multiplikasjonstabellen");
-        String input = in.nextLine();
         int gange = 0;
-        try{
-            gange = Integer.parseInt(input);
-        }catch (NumberFormatException nfe){
-            nfe.printStackTrace();
+        int gangee = 0;
+        while(gange==0){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter 1 Tall fra multiplikasjonstabellen");
+            String input = in.nextLine();
+            try{
+                gange = Integer.parseInt(input);
+            }catch (NumberFormatException nfe){
+                System.out.println("Dette er ikke et valid tall");
+            }
         }
+        while(gangee==0){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter 2 Tall fra multiplikasjonstabellen");
+            String inputt = in.nextLine();
+            try{
+                gangee = Integer.parseInt(inputt);
+            }catch (NumberFormatException nfe){
+                System.out.println("Dette er ikke et valid tall");
+        }
+    }
         int resultat;
-            for (int i = 0; i<11; i++){
-                resultat=gange*i;
-                System.out.println(resultat);
+        int resultatt;
+        int i = 0;
+        int u = 0;
+        int g = 0;
             
-        }
+            while(i<10){
+                i++;
+                resultat=gange*i;
+                System.out.println(gange + " * " + i + " = " + resultat);
+            }
+            while(i>=10 && u<10){
+                u++;
+                resultatt=gangee*u;
+                System.out.println(gangee + " * " + u + " = " + resultatt);
+            }
     }
 }
--- oving3_2.java
+++ oving3_2.java
@@ -3,19 +3,80 @@ class oving3_2{
     public static void main(String[] args){
         for(int i=10; i>1; i++){
             Scanner in = new Scanner(System.in);
-            System.out.println("Do You Want To Analyse A Number? [1 = Yes / 2 = No]");
-            int answer = in.nextInt();
+            int answer = 0;
+
+            while(answer==0){           //! Første sjekk, gir du input som f,eks "k", kjører denne igjen //
+                System.out.println("Do You Want To Analyse A Number? [1 = Yes / 2 = No]");
+                String svarstring = in.nextLine();
+                try{
+                    answer = Integer.parseInt(svarstring);
+                }catch (NumberFormatException nfe){
+                    System.out.println("Dette er ikke et valid input");
+                }
+            }
+            if(answer != 2 && answer != 1){ //! Gir du svar som er en int men ikke 1 | 2 går koden ned her //
+                int answerr = 0;
+                System.out.println("Dette er ikke et valid input");
+                System.out.println("Do You Want To Analyse A Number? [1 = Yes / 2 = No]");
+                String svarstringg = in.nextLine();
+                try{
+                    answerr = Integer.parseInt(svarstringg);
+                }catch (NumberFormatException nfe){
+                    System.out.println("Dette er ikke et valid input");
+                }
+                //*                     SPACE                         //
+                //*                     SPACE                         //
+                //*                     SPACE                         //
+                //*                     SPACE                         //
+                //*                     SPACE                         //
+                //*                     SPACE                         //
+                if(answerr != 2 && answerr != 1){
+                    System.out.println("Dette er ikke et valid input");
+                }
+                if(answerr == 2){
+                    break;
+                }else if(answerr == 1){
+                    System.out.println("Enter Number For Analysis");
+                    String input = ("");
+                    input = in.nextLine();
+                    try{
+                        double numbers = Double.parseDouble(input);
+                        if(numbers%2 == 0 && numbers != 2){
+                            System.out.println("The Entered Number Is Not A Prime Number");
+                        }else if((numbers%2 != 0 && numbers>1) || numbers==2){
+                            System.out.println("The Entered Number Is A Prime Number");
+                        }else{
+                            System.out.println("The Entered Number Is Not A Prime Number");
+                        }
+                    }catch (NumberFormatException nfe){
+                        System.out.println("Dette er ikke et valid input");
+                    }
+                }
+            }
+            //*                     SPACE                         //
+            //*                     SPACE                         //
+            //*                     SPACE                         //
+            //*                     SPACE                         //
+            //*                     SPACE                         //
+            //*                     SPACE                         //
+            //*                     SPACE                         //
             if(answer == 2){
                 break;
             }else if(answer == 1){
                 System.out.println("Enter Number For Analysis");
-                double number = in.nextDouble();
-                if(number%2 == 0){
-                    System.out.println("The Entered Number Is Not A Prime Number");
-                }else if(number%2 != 0 && number>1){
-                    System.out.println("The Entered Number Is A Prime Number");
-                }else{
-                    System.out.println("The Entered Number Is Not A Prime Number");
+                String input = ("");
+                input = in.nextLine();
+                try{
+                    double numbers = Double.parseDouble(input);
+                    if(numbers%2 == 0 && numbers != 2){
+                        System.out.println("The Entered Number Is Not A Prime Number");
+                    }else if((numbers%2 != 0 && numbers>1) || numbers==2){
+                        System.out.println("The Entered Number Is A Prime Number");
+                    }else{
+                        System.out.println("The Entered Number Is Not A Prime Number");
+                    }
+                }catch (NumberFormatException nfe){
+                    System.out.println("Dette er ikke et valid input");
                 }
             }
         }
