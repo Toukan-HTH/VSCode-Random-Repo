468f9a31d1a54ade71225daf088f65d07b5d1a5e
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-09-12 22:53:22 +0200

    start 4_2

---
+++ oving4_2.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+class Player1{
+    private double antallKast;
+    private double poeng;
+    private double ferdig;
+    private double terningkast;
+
+    public Player1(double antallKast, double poeng, double ferdig) {
+        this.antallKast=antallKast;
+        this.poeng=poeng;
+        this.ferdig=ferdig;
+    }
+    public double getSumPoeng(double terningkast, double sum){
+        sum=sum+terningkast;
+        return sum;
+    }
+    public double kastTerning(double terningkast){
+        return terningkast;
+    }
+    public double erFerdig(double sum){
+        return sum;
+    }
+}
+
+
+class Player2{
+    private double antallKast;
+    private double poeng;
+    private double ferdig;
+
+    public Player2(double antallKast, double poeng, double ferdig) {
+        this.antallKast=antallKast;
+        this.poeng=poeng;
+        this.ferdig=ferdig;
+    }
+}
