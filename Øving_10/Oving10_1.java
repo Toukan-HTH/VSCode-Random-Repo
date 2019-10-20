5f7f90ce3567c8df9183449a2e583a38f68ae271
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-10-20 20:40:30 +0200

    start øving 10 1

---
+++ Øving_10/Oving10_1.java
@@ -0,0 +1,9 @@
+class Oving10_1{
+    public static void main(String[] args){
+        ArrangementRegister doot = new ArrangementRegister();
+        doot.registrerArrangement(293497, "Reikjavik", "Henrik", "Fest", 1905993972);
+        doot.registrerArrangement(293597, "Reikjavik", "Henrik", "Fest", 1905993972);
+        //doot.registrerArrangement(293497, "Reikjavik", "Henrik", "Fest", 1905993972);
+        //doot.finnArangementerVGS("Reikjavik");
+    }
+}
