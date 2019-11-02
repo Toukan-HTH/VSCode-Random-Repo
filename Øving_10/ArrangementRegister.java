c3efeea0019593aef8e7f472fc36404f47294df0
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-11-03 00:37:09 +0100

    Øving 10_1 done

--- Øving_10/ArrangementRegister.java
+++ Øving_10/ArrangementRegister.java
@@ -14,10 +14,10 @@ class ArrangementRegister{
     public void finnArangementerVGS(String sted){
         int tjener=0;
         for(int i=0;i<arrangementer.size();i++){
-            if(arrangementer.get(i).getSted()==sted){
+            if(arrangementer.get(i).getSted().equals(sted)){
                 tjener++;
                 System.out.println("\nArrangement nummer: " + tjener);
-                arrangementer.get(i).toString();
+                System.out.println(arrangementer.get(i).tostring());
             }
         }
     }
@@ -30,10 +30,11 @@ class ArrangementRegister{
         int tjener=0;
         int size=arrangementer.size();
         for(int i=0;i<size;i++){
-            if(arrangementer.get(i).getTidspunkt()==dato){
+            String p = arrangementer.get(i).getTidspunkt().substring(0,arrangementer.get(i).getTidspunkt().length()-4);
+            if(p.equals(dato)){
                 tjener++;
                 System.out.println("\nArrangement nummer: " + tjener);
-                arrangementer.get(i).toString();
+                System.out.println(arrangementer.get(i).tostring());
             }
         }
     }
@@ -46,22 +47,28 @@ class ArrangementRegister{
 
 
 
-    public void registrerArrangement(String nummer, String sted, String arrangor, String type, String tidspunkt){
+    public boolean registrerArrangement(String nummer, String sted, String arrangor, String type, String tidspunkt){
         int s =0;
+        boolean statement = true;
         while(s==0){
             for(int i=0; i<arrangementer.size();i++){
-                if(nummer==arrangementer.get(i).getNummer()){
+                if(nummer.equals(arrangementer.get(i).getNummer())){
                     System.out.println("Dette Nummeret er i bruk");
+                    boolean statement1=false;
                     s++;
+                    return statement1;
                 }
             }
             arrangementer.add(new Arrangement(nummer, sted, arrangor, type, tidspunkt));
             s++;
         }
+        return statement;
     }
 
 
-    public void sort(){
+    public void sortering(){
+        SorteringEtterTid sort = new SorteringEtterTid();
+        Collections.sort(arrangementer, sort);
         int size = arrangementer.size();
         Collections.sort(this.arrangementer);
         for(int i=0; i<size;i++){
@@ -70,12 +77,26 @@ class ArrangementRegister{
     }
 
 
-    public void sort2(){
+
+
+
+
+
+
+    public void finnArangementerTidsintervall(String dato1, String dato2){
+        ArrayList<Arrangement> arrangementerNEW = new ArrayList<Arrangement>();
+        int dato1INT = Integer.parseInt(dato1.substring(0,dato1.length()));
+        int dato2INT = Integer.parseInt(dato2.substring(0,dato2.length()));
+        for(int i=0;i<arrangementer.size();i++){
+            int p = Integer.parseInt(arrangementer.get(i).getTidspunkt().substring(0,arrangementer.get(i).getTidspunkt().length()-4));
+            if(p>dato1INT && p<dato2INT){
+                arrangementerNEW.add(arrangementer.get(i));
+            }
+        }
         SorteringEtterTid sort = new SorteringEtterTid();
-        Collections.sort(arrangementer, sort);
-        int size = arrangementer.size();
-        for(int i=0; i<size;i++){
-            System.out.println(arrangementer.get(i).tostring());
+        Collections.sort(arrangementerNEW, sort);
+        for(int i=0; i<arrangementerNEW.size();i++){
+            System.out.println(arrangementerNEW.get(i).tostring());
         }
     }
 }
--- Øving_10/Oving10_1.java
+++ Øving_10/Oving10_1.java
@@ -1,8 +1,9 @@
+import java.util.*; 
 class Oving10_1{
     public static void main(String[] args){
         ArrangementRegister doot = new ArrangementRegister();
         doot.registrerArrangement("111345", "by1", "Henrik", "fest", "201911301800");
-        doot.registrerArrangement("222234", "by1", "Geir", "ikke fest", "201909301700");
+        doot.registrerArrangement("222234", "by1", "Geir", "ikke fest", "201911301700");
         doot.registrerArrangement("333253", "by2", "Henrik", "ikke fest", "201908301300");
         doot.registrerArrangement("334123", "by1", "Henrik", "fest", "201905301100");
         doot.registrerArrangement("335764", "by1", "Henrik", "fest", "201907251800");
@@ -14,8 +15,106 @@ class Oving10_1{
         doot.registrerArrangement("226456", "by1", "Geir", "fest", "201904021600");
 
         //doot.finnArangementerVGS("by1");
-        doot.sort2();
-        System.out.println("");
-        doot.sort();
+        //doot.sort2();
+        //System.out.println("");
+        //doot.sortering();
+
+
+        //doot.finnArangementerVGD("20191130");
+
+
+        //doot.finnArangementerTidsintervall("20180101", "20200101");
+
+
+
+
+
+        //* MENYSTYRT KLIENTPROGRAM //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        int program = 0;
+        Scanner sc = new Scanner(System.in);
+        while(program==0){
+            int scanner = 0;
+            int svar=0;
+            //* Scanner input fra valgmeny ///////////////////////////////////////////////////////////////////////////
+            while(scanner==0){
+                System.out.println("\nAlternativer\n 1: Registrere nytt arrangement\n 2: Finne alle arrangementene gitt en dato\n 3: Finne alle arrangementene gitt et sted\n 4: Finne alle arrangementene innenfor et tidsintervall\n 5: Lage liste over alle arrangementene og sortere\n 6: Avslutt");
+                String svarstring = sc.nextLine();
+                Scanner skanner = new Scanner(svarstring);
+                if(skanner.hasNextInt()){
+                    svar=Integer.parseInt(svarstring);
+                    scanner++;
+                }else{
+                    System.out.println("Ikke valid input");
+                }
+            }
+
+
+
+            switch(svar){
+                case 1:
+                System.out.println("Input valid nummer : 6 siffre for eksempel 123456");
+                String nummer = sc.nextLine();
+                System.out.println("Input valid sted : for eksempel by1");
+                String sted = sc.nextLine();
+                System.out.println("Input valid arrangor : for eksempel Geir");
+                String arrangor = sc.nextLine();
+                System.out.println("Input valid type : for eksempel fest");
+                String type = sc.nextLine();
+                System.out.println("Input valid tidspunkt : i format [YYYYMMDDTTTT]");
+                String tidspunkt = sc.nextLine();
+                boolean statement=doot.registrerArrangement(nummer, sted, arrangor, type, tidspunkt);
+                if(statement==true){
+                    System.out.println("Registrert!");
+                }else{
+                    System.out.println("ERROR!");
+                }
+                break;
+
+
+                case 2:
+                System.out.println("Finn alle arrangementene ved hvilken dato? : [YYYYMMDD]");
+                String svarDato = sc.nextLine();
+                doot.finnArangementerVGD(svarDato);
+                break;
+
+
+                case 3:
+                System.out.println("Finn alle arrangementene ved hvilket sted?");
+                String stedSvar = sc.nextLine();
+                doot.finnArangementerVGS(stedSvar);
+                break;
+
+                case 4:
+                System.out.println("Finn alle arrangementene innen tidsintervallet:");
+                System.out.println("Fra dato? [YYYYMMDD]");
+                String fraDato = sc.nextLine();
+                System.out.println("Til dato? [YYYYMMDD]");
+                String tilDato = sc.nextLine();
+                doot.finnArangementerTidsintervall(fraDato, tilDato);
+                break;
+
+
+                case 5:
+                doot.sortering();
+                break;
+
+
+                case 6:
+                program++;
+                break;
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }
