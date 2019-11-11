a6ea23faed5e65fb3111f5c25cea18abd2cda5a2
Toukan-HTH <Henrik.T.H@Outlook.com>
2019-11-11 21:23:52 +0100

    k

--- Øving_11/Klient.java
+++ Øving_11/Klient.java
@@ -4,10 +4,10 @@ class Klient{
         EiendomRegister doot = new EiendomRegister();
         Scanner sc = new Scanner(System.in);
         doot.registrerEiendom(1445,"Gloppen",77,631,"",1017.6,"Jens Olsen");
-        doot.registrerEiendom(1445,"Gloppen",77,131,"Syningom",661.3,"Jens Olsen");
-        doot.registrerEiendom(1445,"Gloppen",75,19,"Fugletun",650.6,"Jens Olsen");
-        doot.registrerEiendom(1445,"Gloppen",74,188,"",1457.2,"Jens Olsen");
-        doot.registrerEiendom(1445,"Gloppen",69,47,"Høiberg",1339.4,"Jens Olsen");
+        doot.registrerEiendom(1445,"Gloppen",77,131,"Syningom",661.3,"Nicolay Madsen");
+        doot.registrerEiendom(1445,"Gloppen",75,19,"Fugletun",650.6,"Evilyn Jensen");
+        doot.registrerEiendom(1445,"Gloppen",74,188,"",1457.2,"Karl Ove Bråten");
+        doot.registrerEiendom(1445,"Gloppen",69,47,"Høiberg",1339.4,"Elsa Indregård");
         //doot.registrerEiendom(1445,"Gloppen",77,631,"",500.6,"Jens Olsen");
         //String test = sc.nextLine();
         //doot.registrerEiendom(1445,"Gloppen",77,631,test,1017.6,"Jens Olsen");
