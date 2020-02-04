a3762f32b3d7bc5278b27881e1dcea9f9bb090e0
Toukan-HTH <Henrik.T.H@Outlook.com>
2020-02-04 17:46:03 +0100

    bare klient program igjen

--- Obligatorisk_2/MemberArchive.java
+++ Obligatorisk_2/MemberArchive.java
@@ -7,8 +7,20 @@ class MemberArchive{
         members = new ArrayList<BonusMember>();
     }
 
-    // public boolean registerPoints(int membernr, int poeng){
-    // }
+    public boolean registerPoints(int membernr, int poeng){
+        boolean result = true;
+        for(int i =0;i<members.size();i++){
+            if(membernr==members.get(i).getMemberNo()){
+                members.get(i).registerPoints(poeng);
+                result = false;
+            }
+        }
+        if(!result){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     public int newMember(Personals personalstuff, LocalDate enrollmentDate) {
         int membernr = findAvailableNo();
@@ -16,12 +28,63 @@ class MemberArchive{
         return membernr;
     }
 
-    private static int findAvailableNo(){
-        return 1;
+    private int findAvailableNo(){
+        boolean loop = true;
+        int random =0;
+        if(loop){
+            random = (int)(Math.random() * ((99999-0+1)))+0;
+            for(int i =0; i<members.size();i++){
+                if(random==members.get(i).getMemberNo()){
+                    loop = false;;
+                }
+            }
+            if(!loop){
+                loop=true;
+            }else{
+                loop=false;
+            }
+        }
+        return random;
     }
 
-    //public int findPoints(String password, int membernr){
-    //}
+    public int findPoints(String password, int membernr){
+        int result = -1;
+        for(int i = 0; i<members.size();i++){
+            if(membernr==members.get(i).getMemberNo()){
+                if(members.get(i).okPassword(password)){
+                    result=members.get(i).getPoints();
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+    public void checkMembers(){
+        for(int i = 0; i<members.size();i++){
+
+            if(members.get(i).findQualificationPoints(LocalDate.now())>=25000 && members.get(i).findQualificationPoints(LocalDate.now())<=74999){
+                members.set(i, new SilverMember(members.get(i).getMemberNo(), members.get(i).getPersonals(), members.get(i).getEnrolledDate()));
+            }
+
+            if(members.get(i).findQualificationPoints(LocalDate.now())>=75000){
+                members.set(i, new GoldMember(members.get(i).getMemberNo(), members.get(i).getPersonals(), members.get(i).getEnrolledDate()));
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
 
 
 
